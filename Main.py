a60bb41d77db5d1f91202260a41083060bcd856a
weiwei <wxjlmr@126.com>
2018-06-22 07:57:35 +0800

    0.1 搭建ui框架

---
+++ Main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from MainForm import *
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    ftpw = MainWindow()
+    ftpw.show()
+
+    sys.exit(app.exec_())
+
+
+
